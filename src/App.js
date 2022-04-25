3de3ac69daa0a7f209b72dfa8bf9285e0c14ea63
hebercruz384 <73669492+hebercruz384@users.noreply.github.com>
2022-04-25 18:20:43 -0400

    created slot

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ export default function App() {
     <div className="App">
       <h1>Hello My React App</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <Slot />
     </div>
+    
+  );
+}
+function Slot() {
+  return(
+    <div style= {{
+      backgroundColor : 'blue',
+      width : '100px',
+      lenght : '100px'
+ }}
+    /> 
+    
   );
 }
