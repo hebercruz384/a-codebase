529444cbfb470d804064fc1ba32b2d5152c2cc1e
hebercruz384 <73669492+hebercruz384@users.noreply.github.com>
2022-05-09 18:41:10 -0400

    Created a state variable to track the player turn and used conditonals and event handlers to display the player turn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [playerTurn, changePlayerTurn] = useState("red");
+  function togglePlayerTurn() {
+    if (playerTurn === "red") {
+      changePlayerTurn("yellow");
+    } else {
+      changePlayerTurn("red");
+    }
+  }
   return (
     <div className="App">
-      <div className="slotA">
-        <div className="inner-slot"></div>
+      <div className="slotA" onClick={togglePlayerTurn}>
+        <div className="inner-slot red yellow"></div>
       </div>
+      <p>It is {playerTurn}'s turn</p>
     </div>
   );
 }
