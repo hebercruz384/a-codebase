43fc91c46e88665f7fbec76280858bc62f076aaf
hebercruz384 <73669492+hebercruz384@users.noreply.github.com>
2022-05-09 17:29:19 -0400

    Made one slot using css and divs

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello My React App</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <Slot />
+      <div className="slotA">
+        <div className="inner-slot"></div>
+      </div>
     </div>
-    
-  );
-}
-function Slot() {
-  return(
-    <div style= {{
-      backgroundColor : 'blue',
-      width : '100px',
-      lenght : '100px'
- }}
-    /> 
-    
   );
 }
